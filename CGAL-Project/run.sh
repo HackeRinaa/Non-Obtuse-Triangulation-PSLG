7ccaf6b45befb91bc43779afbb3365df194ce309
VasilisFoutris <foutrisbill@gmail.com>
2025-02-12 18:12:55 +0200

    results for all categories

--- CGAL-Project/run.sh
+++ CGAL-Project/run.sh
@@ -9,4 +9,7 @@ cd built
 cmake ..
 make -j4
 
-./triangulation_program ../data/convexClosedConstraints/input_convex_closed_constraints_hybrid.json ../data/convexClosedConstraints/output_convex_closed_constraints_hybrid.json
+./triangulation_program ../data/nonConvexParallel/input_non_convex_parallel_aco.json ../data/nonConvexParallel/output_non_convex_parallel_aco.json
+./triangulation_program ../data/nonConvexParallel/input_non_convex_parallel_local.json ../data/nonConvexParallel/output_non_convex_parallel_local.json
+./triangulation_program ../data/nonConvexParallel/input_non_convex_parallel_sim_an.json ../data/nonConvexParallel/output_non_convex_parallel_sim_an.json
+./triangulation_program ../data/nonConvexParallel/input_non_convex_parallel_hybrid.json ../data/nonConvexParallel/output_non_convex_parallel_hybrid.json
